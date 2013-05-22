18c4ff3c9150d712aa89798e4ec4921ce6f576f7
Dusty Jewett <dusty.jewett@gmail.com>
2013-05-22 11:20:46 -0700

    Fixed an issue that seems to be caused by a deployd bug
    It seems to mistakenly identify these queries as recursive.... by setting a high limit, it lets these queries complete.

--- resources/report-results/get.js
+++ resources/report-results/get.js
@@ -1,9 +1,21 @@
 
-var result = this;
-
-dpd.diffs.get({ $or:[
+this.diffs = [];
+dpd.diffs.get(
+    { $or:[
         {reportResultA: this.id},
         {reportResultB: this.id}
-]}, function(diffs) {
-    result.diffs = diffs;
+        ], 
+    /** This seems like a bug, this certainly isn't a recursive call, but it fails without a high limit */
+    $limitRecursion: 255
+    
+    }, function(diffs, error) {
+    if(error){
+        console.log("Error:", error);
+    }
+    if(diffs){
+        this.diffs = diffs;
+        this.numDiffs = diffs.length;
+    } else {
+        console.log("No Diffs loaded for: " + this.id);
+    }
 });
--- resources/reports/get.js
+++ resources/reports/get.js
@@ -1,10 +1,11 @@
+console.log("----------- Get Report: " + this.id + " -------------");
 if(this.masterResult){
     dpd.reportresults.get({id:this.masterResult},function(result){
         this.masterResult = result;
     });
 }
 if(query.include === 'results') {
-    
+
   dpd.reportresults.get({report: this.id}, function(results) {
     this.results = results;
   });
