52394dc7f25f8f11d763f227d61b395575554657
Dusty Jewett <dusty.jewett@gmail.com>
2013-06-07 13:39:35 -0700

    Adding "Watchers" to the batch model and UI

--- api/models/batch.js
+++ api/models/batch.js
@@ -1,6 +1,7 @@
 module.exports = function(mongoose){
     var BatchSchema = new mongoose.Schema({
         name:{type:String},
+        watchers:[{type:String}],
         reports:[{type : mongoose.Schema.ObjectId, ref : 'reports'}],
         results:[{type : mongoose.Schema.ObjectId, ref : 'batch-results'}],
         scheduleEnabled:{type:Boolean, default:false},
