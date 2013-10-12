686e52122a483e47ce34a9981b40da178f9b5bdb
Dusty Jewett <dusty.jewett@gmail.com>
2013-10-12 11:32:08 -0700

    Adding /healthcheck route

--- api/routes/monitoringRoutes.js
+++ api/routes/monitoringRoutes.js
@@ -1,5 +1,23 @@
 module.exports = function(server){
     'use strict';
+    var os = require('os');
+
+    server.get('/healthcheck', function(req, res, next) {
+        res.send({
+            alive:true,
+            hostname:os.hostname(),
+            type:os.type(),
+            platform:os.platform(),
+            arch:os.arch(),
+            release:os.release(),
+            uptime:os.uptime(),
+            load:os.loadavg(),
+            totalMemory:os.totalmem(),
+            freeMemory:os.freemem(),
+            cpus:os.cpus()
+        });
+        return next();
+    });
 
     server.get('/monitoring', function(req, res, next) {
         res.send({
